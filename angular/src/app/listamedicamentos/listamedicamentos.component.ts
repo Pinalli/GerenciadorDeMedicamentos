eafb3dd60545232ef42c4ba8d51004d189cd690b
Lucas Bertoglio Ciocari <lucas.ciocari@ibm.com>
2019-04-29 10:21:15 -0300

    organizing packages in both api and angular

---
+++ angular/src/app/listamedicamentos/listamedicamentos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MedicamentosService } from '../services/medicamentos.service';
+
+@Component({
+  selector: 'app-medicamentos',
+  templateUrl: './medicamentos.component.html',
+  styleUrls: ['./medicamentos.component.sass']
+})
+export class MedicamentosComponent implements OnInit {
+
+    medicamentos: any = [];
+  constructor(private service: MedicamentosService) { }
+
+  ngOnInit() {
+      this.service.listarMedicamentos().subscribe(
+          (data: any) => {
+              const results = data;
+              for (const medicamento of results) {
+                  this.medicamentos.push(medicamento);
+              }
+          }
+      );
+  }
+
+}
