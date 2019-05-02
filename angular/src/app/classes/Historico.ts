e7bc6a89bdd52bd5eb3ced5e7784391027cfe8aa
Lucas Bertoglio Ciocari <lucas.ciocari@ibm.com>
2019-05-02 15:46:44 -0300

    added angular classes

---
+++ angular/src/app/classes/Historico.ts
@@ -0,0 +1,6 @@
+export class Historico{
+  id: number;
+  idmedicamento: number;
+  idpaciente: number;
+  iddoenca: number;
+}
---
+++ angular/src/app/classes/Pacientes.ts
@@ -0,0 +1,11 @@
+export class Pacientes{
+  id: number;
+  nome: string;
+  genero: string;
+  doenca1: number;
+  medicamento1: number;
+  doenca2: number;
+  medicamento2: number;
+  doenca3: number;
+  medicamento3: number;
+}
